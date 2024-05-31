ccba9a65069328454d55d41729eef43f1dc4b1af
Kenny <lin.kenn@northeastern.edu>
2024-05-30 23:43:54 -0400

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Kenny's Portfolio",
+  description: "Kenny's personal portfolio website created using NextJs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <h1 className="text-4xl text-white font-bold">Hello, World!</h1>
+    </main>
+  );
+}
