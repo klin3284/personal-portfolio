9616d445159903666b35ac99c078431eebb0a43d
Kenny <lin.kenn@northeastern.edu>
2024-05-31 21:07:13 -0400

    hero component with pfp

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "@components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center overflow-hidden mx-auto sm:px-10 px-5 w-screen h-screen">
-      <Hero/>
+    <main className="relative bg-gray-900 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5 w-screen h-screen">
+      <Hero />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import Image from 'next/image';
 import { Meteors } from "./ui/meteors";
 
 const Hero = () => {
     return (
         <div className='relative p-0.5'>
-            <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-                <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-                    Hi, I&apos;m Kenny
+            <div className="relative bg-gray-900 px-4 py-20 w-[50vw] h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
+                <div style={{ width: '20vw', height: '20vw', position: 'relative', zIndex: 1, marginBottom: 20 }}>
+                    <Image src="/pfp_cropped.png" alt="Kenny's Hero Image" layout="fill" objectFit="cover" style={{ borderRadius: "50%" }} />
+                </div>
+
+                <h1 className="font-bold text-4xl text-white relative z-50 mb-2">
+                    Kenny Lin
                 </h1>
 
-                <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-                    Here is me experimenting with this meteor effect from Aceternity
-                </p>
+                <div className="relative items-center flex flex-col">
+                    <h2 className="font-bold text-base text-white mb-0">
+                        Junior Software Engineer
+                    </h2>
+                    <h2 className="font-normal text-base text-slate-500 text-center">
+                        Northeastern Unversity 2024<br/>
+                        Computer Engineering and Computer Science
+                    </h2>
+
+                </div>
 
                 {/* Meaty part - Meteor effect */}
                 <Meteors number={20} />
