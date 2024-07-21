3dbc33f364ccafe8595910789a509e401a1189c6
Kenny Lin <lin.kenn@northeastern.edu>
2024-07-21 13:39:30 -0400

    github link typo

--- components/SocialBar.tsx
+++ components/SocialBar.tsx
@@ -4,7 +4,7 @@ import Icon from './icons';
 const SocialBar = () => {
     return (
         <div className="grid grid-cols-4 gap-4">
-            <a href="https://github.com/klin4084" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
+            <a href="https://github.com/klin3284" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
                 <Icon name="GitHub" size={40} />
             </a>
             <a href="https://www.linkedin.com/in/kennylin24/" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
