51ee427f89163479de775debc8d1c25312648027
Kenny Lin <lin.kenn@northeastern.edu>
2024-07-21 13:35:38 -0400

    Hero new style changes

--- components/SocialBar.tsx
+++ components/SocialBar.tsx
@@ -4,17 +4,17 @@ import Icon from './icons';
 const SocialBar = () => {
     return (
         <div className="grid grid-cols-4 gap-4">
-            <a href="https://github.com/klin3284" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
-                <Icon name="GitHub" size={32} />
+            <a href="https://github.com/klin4084" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
+                <Icon name="GitHub" size={40} />
             </a>
             <a href="https://www.linkedin.com/in/kennylin24/" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
-                <Icon name="LinkedIn" size={32} />
+                <Icon name="LinkedIn" size={40} />
             </a>
             <a href="" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
-                <Icon name="Resume" size={32} />
+                <Icon name="Resume" size={40} />
             </a>
             <a href="" target="_blank" rel="noopener noreferrer" className="transition-colors duration-200 hover:text-purple-400">
-                <Icon name="Bamboo" size={32} />
+                <Icon name="Bamboo" size={40} />
             </a>
         </div>
     )
