39e341ef85b17c46167df8033b36d6670d673e71
Kenny <lin.kenn@northeastern.edu>
2024-08-03 00:27:29 -0400

    experience initial

--- components/sections/Experiences/index.tsx
+++ components/sections/Experiences/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
 import Section from "../index";
 
 interface ExperienceItemProps {
@@ -8,6 +11,7 @@ interface ExperienceItemProps {
   startDate: Date;
   endDate: Date;
   description: string[];
+  logo: string;
 }
 
 const ExperienceItem: React.FC<ExperienceItemProps> = ({
@@ -17,22 +21,53 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({
   startDate,
   endDate,
   description,
-}) => (
-  <div className="mb-8 border-l-2 border-gray-200 pl-4">
-    <h3 className="text-xl font-bold text-blue-400">{company}</h3>
-    <p className="text-lg">{title}</p>
-    <p className="text-sm">{location}</p>
-    <p className="text-sm">
-      {startDate.toLocaleDateString()} - {endDate.toLocaleDateString()}
-    </p>
-    <p className="mt-2 text-gray-700">{description}</p>
-  </div>
-);
+  logo,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+  };
+
+  const toggleCard = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      className="mb-8 p-4 cursor-pointer hover:shadow-lg hover:scale-105 transition-transform duration-300"
+      onClick={toggleCard}
+    >
+      <div className="bg-gray-700 rounded-xl px-2 py-1 inline-block mb-4">
+        <p className="text-xs font-bold text-white">
+          {startDate.toLocaleDateString(undefined, dateOptions)} -{" "}
+          {endDate.toLocaleDateString(undefined, dateOptions)}
+        </p>
+      </div>
+      <h3 className="text-xl font-bold text-blue-400">
+        {company.toUpperCase()}
+      </h3>
+      <p className="text-lg">{title}</p>
+      <p className="text-sm">{location}</p>
+      {isOpen && (
+        <div className="mt-4 p-4 bg-white rounded-lg shadow-lg">
+          <ul className="text-gray-700">
+            {description.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+          <Image src={logo} alt={company} width={100} height={100} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 const Experiences = () => {
   const jobs: ExperienceItemProps[] = [
     {
-      company: "Avidyne",
+      company: "Avidyne Corporation",
       location: "Concord, MA",
       title: "Software Engineer",
       startDate: new Date("07-01-2022"),
@@ -41,9 +76,10 @@ const Experiences = () => {
         "Developed software for avionics systems",
         "Worked on a team of 5 engineers",
       ],
+      logo: "/avidyne_logo.png",
     },
     {
-      company: "MCPHS",
+      company: "Massachusetts College of Pharmacy and Health Sciences",
       location: "Boston, MA",
       title: "Applications Developer",
       startDate: new Date("07-14-2023"),
@@ -52,9 +88,10 @@ const Experiences = () => {
         "Developed software for the university",
         "Worked on a team of 3 developers",
       ],
+      logo: "/mcphs_logo.png",
     },
     {
-      company: "Asian American Center",
+      company: "Asian American Center - Northeastern",
       location: "Boston, MA",
       title: "Intern",
       startDate: new Date("09-02-2021"),
@@ -63,6 +100,7 @@ const Experiences = () => {
         "Developed software for the university",
         "Worked on a team of 5 engineers",
       ],
+      logo: "/aac_logo.png",
     },
   ];
 
