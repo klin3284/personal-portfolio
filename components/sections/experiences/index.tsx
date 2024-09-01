2638c8d95f1b0a88790ad91b5deb0fb66a8726c9
Kenny <lin.kenn@northeastern.edu>
2024-08-31 22:51:12 -0400

    proper company logos

--- components/sections/experiences/index.tsx
+++ components/sections/experiences/index.tsx
@@ -10,21 +10,21 @@ const Experiences = () => {
       title: "Software Engineer",
       startDate: new Date("07-01-2022"),
       endDate: new Date("12-18-2022"),
-      logo: "/avidyne_logo_circle.png",
+      logo: "/companyLogos/avidyne_logo_circle.png",
     },
     {
       company: "Massachusetts College of Pharmacy and Health Sciences",
       title: "Applications Developer",
       startDate: new Date("07-14-2023"),
       endDate: new Date("12-21-2023"),
-      logo: "/mcphs_logo_circle.png",
+      logo: "/companyLogos/mcphs_logo_circle.png",
     },
     {
       company: "Asian American Center - Northeastern",
       title: "Intern",
       startDate: new Date("09-02-2021"),
       endDate: new Date("05-06-2024"),
-      logo: "/aac_logo_circle.png",
+      logo: "/companyLogos/aac_logo_circle.png",
     },
   ];
 
