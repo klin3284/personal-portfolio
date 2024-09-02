a311640880a16a922de4ab771c38ec3366b8bcd2
Kenny <lin.kenn@northeastern.edu>
2024-09-02 00:17:04 -0400

    date ISO 8601 format for compatibility

--- components/sections/experiences/index.tsx
+++ components/sections/experiences/index.tsx
@@ -8,22 +8,22 @@ const Experiences = () => {
     {
       company: "Avidyne",
       title: "Software Engineer",
-      startDate: new Date("07-01-2022"),
-      endDate: new Date("12-18-2022"),
+      startDate: new Date("2022-07-01"),
+      endDate: new Date("2022-12-18"),
       logo: "/companyLogos/avidyne_logo_circle.png",
     },
     {
       company: "Massachusetts College of Pharmacy and Health Sciences",
       title: "Applications Developer",
-      startDate: new Date("07-14-2023"),
-      endDate: new Date("12-21-2023"),
+      startDate: new Date("2023-07-14"),
+      endDate: new Date("2023-12-21"),
       logo: "/companyLogos/mcphs_logo_circle.png",
     },
     {
       company: "Asian American Center",
       title: "Intern",
-      startDate: new Date("09-02-2021"),
-      endDate: new Date("05-06-2024"),
+      startDate: new Date("2021-09-02"),
+      endDate: new Date("2024-05-06"),
       logo: "/companyLogos/aac_logo_circle.png",
     },
   ];
