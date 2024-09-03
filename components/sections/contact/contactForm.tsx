6f407e221bb2f0c5759a56ff84c5c2811d2b7937
Kenny <lin.kenn@northeastern.edu>
2024-09-02 22:09:00 -0400

    contact section with link grid to access other routes (yet to be made)

---
+++ components/sections/contact/contactForm.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import { contactSchema } from "@schemas/contact";
+import { z } from "zod";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@styles/themeProvider";
+import Icon from "../../icons/index";
+
+const ContactForm = () => {
+  const [, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+
+  const isSmallScreen = useMediaQuery("(max-width: 768px)");
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [id]: false,
+    }));
+
+    if (typeof document !== "undefined") {
+      const btn = document.querySelector("#btn");
+      const btnText = document.querySelector("#btnText");
+      if (!btn || !btnText) {
+        return;
+      }
+      btnText.innerHTML = "Submit";
+      btn.classList.remove("active");
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      contactSchema.parse(formData);
+      const response = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      setFormData({ name: "", email: "", message: "" });
+      setIsSubmitted(true);
+      if (typeof document !== "undefined") {
+        const btn = document.querySelector("#btn");
+        const btnText = document.querySelector("#btnText");
+        if (!btn || !btnText) {
+          return;
+        }
+        btnText.innerHTML = "Thanks";
+        btn.classList.add("active");
+      }
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        const fieldErrors: { [key in "name" | "email" | "message"]: boolean } =
+          { name: false, email: false, message: false };
+        e.errors.forEach((error) => {
+          const fieldName = error.path[0] as "name" | "email" | "message";
+          fieldErrors[fieldName] = true;
+        });
+        setErrors(fieldErrors);
+      }
+    }
+  };
+
+  return (
+    <div
+      id="contactMe"
+      className="flex flex-col rounded-2xl border-2 border-gray-600 pt-4 pb-4 px-6 lg:pt-4 lg:px-8"
+    >
+      <div className="flex gap-2 items-start mb-1">
+        <Icon name="Mail" size={26} />
+        <h2 className="font-semibold text-lg md:text-xl">
+          Get in touch with me!
+        </h2>
+      </div>
+      <p className="text-gray-400 mb-6 text-base lg:text-lg">
+        If you have any questions or just want to say hi, feel free to send me a
+        message!
+      </p>
+      <ThemeProvider theme={theme}>
+        <div className="flex w-full flex-col items-center">
+          <div className="flex flex-col lg:flex-row w-full gap-4">
+            <TextField
+              id="name"
+              label="Name"
+              variant="outlined"
+              value={formData.name}
+              onChange={handleChange}
+              error={errors.name}
+              size={isSmallScreen ? "small" : "medium"}
+              className="flex-1"
+            />
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={formData.email}
+              onChange={handleChange}
+              error={errors.email}
+              size={isSmallScreen ? "small" : "medium"}
+              className="flex-1"
+            />
+          </div>
+          <div className="w-full mt-4">
+            <TextField
+              id="message"
+              label="Message"
+              variant="outlined"
+              multiline
+              rows={4}
+              value={formData.message}
+              onChange={handleChange}
+              error={errors.message}
+              size={isSmallScreen ? "small" : "medium"}
+              className="w-full"
+            />
+          </div>
+          <div className="flex flex-row mt-4 w-full items-center">
+            <p className="text-xs md:text-sm text-gray-400 italic hidden md:block">
+              Powered by ResendApi
+            </p>
+            <p className="text-xs md:text-sm text-gray-400 italic block md:hidden">
+              ResendApi
+            </p>
+            <button
+              id="btn"
+              className="submit-button ml-auto"
+              onClick={handleSubmit}
+            >
+              <p id="btnText">Submit</p>
+              <div className="check-box">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                  <path
+                    fill="transparent"
+                    d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                    style={{
+                      strokeWidth: 8,
+                      stroke: "#fff",
+                      strokeDasharray: 34,
+                      strokeLinecap: "round",
+                    }}
+                  />
+                </svg>
+              </div>
+            </button>
+          </div>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ components/sections/contact/linkGrid.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import ScrollToSectionButton from "@components/layout/scrollToSectionButton";
+
+const LinkGrid = () => (
+  <div className="grid grid-cols-3 gap-4 w-full">
+    <div className="flex flex-col">
+      <h3 className="font-bold text-md sm:text-lg mb-6">GENERAL</h3>
+      <ul className="list">
+        <li className="listItem">
+          <ScrollToSectionButton
+            sectionId="home"
+            label="Home"
+            className="link"
+          />
+        </li>
+        <li className="listItem">
+          <ScrollToSectionButton
+            sectionId="about"
+            label="About"
+            className="link"
+          />
+        </li>
+        <li className="listItem">
+          <ScrollToSectionButton
+            sectionId="projects"
+            label="Projects"
+            className="link"
+          />
+        </li>
+        <li className="listItem">
+          <ScrollToSectionButton
+            sectionId="blog"
+            label="Blog"
+            className="link"
+          />
+        </li>
+      </ul>
+    </div>
+    <div className="flex flex-col">
+      <h3 className="font-bold text-md sm:text-lg mb-6">SPECIFICS</h3>
+      <ul className="list">
+        <li className="listItem">
+          <a href="https://github.com/klin3284" className="link">
+            Github
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="https://www.linkedin.com/in/kennylin24/" className="link">
+            LinkedIn
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="credits" className="link">
+            Credits
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="licenses" className="link">
+            Licenses
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className="flex flex-col">
+      <h3 className="font-bold text-md sm:text-lg mb-6">EXTRA</h3>
+      <ul className="list">
+        <li className="listItem">
+          <a href="food" className="link">
+            Food Blog
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="hobbies" className="link">
+            Hobbies
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="computers" className="link">
+            PC Builds
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="https://www.instagram.com/northeastern/" className="link">
+            Go Huskies
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default LinkGrid;
